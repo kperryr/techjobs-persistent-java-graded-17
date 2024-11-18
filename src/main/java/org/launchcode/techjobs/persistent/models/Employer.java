2748d9bb1c05913f8b16efa1e984d901dfecedb5
Katherine <43319584+kperryr@users.noreply.github.com>
2024-11-18 14:07:53 -0500

    final GA4

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,44 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(min=3,max=50, message="Location must be between 3 and 50 characters long")
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(){}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public @NotBlank @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters long") String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotBlank @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters long") String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
